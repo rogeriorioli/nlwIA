d3266d840200110fef07ae73bf5b442f75ee47e5
rogeriorioli <carlos.orioli@instivo.com.br>
2023-09-16 14:25:11 -0300

    final project

---
+++ backend/src/lib/openai.ts
@@ -0,0 +1,6 @@
+import 'dotenv/config'
+import { OpenAI } from 'openai'
+
+export const openai = new OpenAI({
+  apiKey: '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'
+})
---
+++ backend/src/server.ts
@@ -0,0 +1,20 @@
+import fastify from "fastify";
+import {fastifyCors} from '@fastify/cors'
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAiCompletionRoute } from "./routes/generateAICompletionRoute";
+
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin : '*'
+})
+
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
+app.listen({
+  port : 3333,
+}).then(() => console.log('HTTP SERVER RUNNING'))
